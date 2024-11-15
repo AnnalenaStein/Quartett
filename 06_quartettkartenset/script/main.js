d8646ddc34259ca91285772cc89a683ffc1bd855
AnnalenaStein <154338373+AnnalenaStein@users.noreply.github.com>
2024-11-15 21:20:57 +0100

    Änderungen Icons und Dinge funktionieren

--- 06_quartettkartenset/script/main.js
+++ 06_quartettkartenset/script/main.js
@@ -2,14 +2,14 @@ console.log("HERE WE ARE AGAIN");
 
 //Hintergrundfarbe für Gruppen
 const groupColors = {
-    A: '#FF5733',
-    B: '#33FF57', 
-    C: '#3357FF', 
-    D: '#FF5733',
-    E: '#33FF57', 
-    F: '#3357FF', 
-    G: '#3357FF', 
-    H: '#3357FF',    
+    A: '#E63E3E',
+    B: '#53DA41', 
+    C: '#E29C45', 
+    D: '#1E97E8',
+    E: '#6764E7', 
+    F: '#C77E3E', 
+    G: '#9A47DB', 
+    H: '#EFDA38',    
 };
 
 data.forEach(animal => {
@@ -32,42 +32,42 @@ $(document).ready(function () {
                 ${animal.trivia_german}
                 </div>
                 <div class="stat-icon">
-                    <img src="images/icons/weight.jpeg" alt="weight" />
+                    <img src="images/icons/weight.jpg" alt="weight" />
                 </div>
                 <div class="stat-value">4500</div>
 
 
         
                 <div class="stat-icon">
-                    <img src="images/icons/length.jpeg" alt="length" />
+                    <img src="images/icons/length.jpg" alt="length" />
                 </div>
                 <div class="stat-value">400</div>
 
 
           
                 <div class="stat-icon">
-                    <img src="images/icons/age.jpeg" alt="maximum age" />
+                    <img src="images/icons/age.jpg" alt="maximum age" />
                 </div>
                 <div class="stat-value">50</div>
 
 
        
                 <div class="stat-icon">
-                    <img src="images/icons/speed.jpeg" alt="max speed" />
+                    <img src="images/icons/speed.jpg" alt="max speed" />
                 </div>
                 <div class="stat-value">35</div>
 
 
     
                 <div class="stat-icon">
-                    <img src="images/icons/offspring.jpeg" alt="offspring count per cycle" />
+                    <img src="images/icons/offspring.jpg" alt="offspring count per cycle" />
                 </div>
                 <div class="stat-value">1</div>
 
 
              
                 <div class="stat-icon">
-                    <img src="images/icons/death.jpeg" alt="casualties per year" />
+                    <img src="images/icons/death.jpg" alt="casualties per year" />
                 </div>
                 <div class="stat-value">500</div>
 
@@ -77,6 +77,20 @@ $(document).ready(function () {
         </div>`);
 
         $('#wrapper').append(divBox);
+    });
+
+
+    $(document).ready(function () {
+        // Extrahiere eindeutige Gruppennamen
+        let uniqueGroups = data
+            .map(animal => animal.groupname_german) // Gruppennamen extrahieren
+            .filter((value, index, self) => self.indexOf(value) === index); // Doppelte entfernen
+    
+        // Erstelle HTML-Elemente für jede Gruppe
+        uniqueGroups.forEach(groupname => {
+            let groupElement = $(`<div class="group-name">${groupname}</div>`); // Neues Element
+            $('aside').append(groupElement); // Füge es dem aside hinzu
+        });
     });
 
    // Mischen und wieder sortieren
@@ -90,6 +104,10 @@ $(document).ready(function () {
     cards = gmynd.shuffleArray(cards); // Nutze gmynd zum Mischen
     wrapper.innerHTML = ''; // Entfernt alle Karten
     cards.forEach(card => wrapper.appendChild(card)); // Fügt die Karten in zufälliger Reihenfolge hinzu
+    document.querySelector('main').style.gridTemplateColumns = '1fr';
+    document.querySelector('aside').style.display = 'none';
+    wrapper.style.background = 'radial-gradient( #F1B05F, #65B0E1, #4D4C9F, #7A624D, #8C4EBC, #E3D77A, #DB5151, #84F375)';
+
 }
    
    // Funktion zum Zurücksetzen der Reihenfolge
@@ -98,15 +116,21 @@ $(document).ready(function () {
        wrapper.innerHTML = '';
        originalOrder.forEach(card => wrapper.appendChild(card));
        cards = Array.from(originalOrder); // Stellt sicher, dass `cards` auch die ursprüngliche Reihenfolge hat
+       document.querySelector('main').style.gridTemplateColumns = '70px 1fr';
+       document.querySelector('aside').style.display = 'grid';
+       wrapper.style.background = 'linear-gradient(to bottom, #DB5151, #84F375, #F1B05F, #65B0E1, #4D4C9F, #7A624D, #8C4EBC, #E3D77A)';
    }
    
    // Toggle-Funktion zum Wechseln zwischen Zufällig und Aktuell
    function toggleOrder() {
        isRandomOrder = !isRandomOrder;
+       const button = document.getElementById('shuffleButton'); // Button-Element auswählen
        if (isRandomOrder) {
            shuffleCards();
+           button.innerText = 'sortieren';
        } else {
            resetOrder();
+           button.innerText = 'mischen';
        }
    }
 
